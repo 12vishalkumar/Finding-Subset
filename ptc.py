8cbd213072a6bfa5e51035686512e1f14d67daff
12vishalkumar <90981381+12vishalkumar@users.noreply.github.com>
2022-04-18 15:26:11 +0530

    Add files via upload

---
+++ ptc.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+T = int(input())
+for i in range(T):
+    sum = 0
+    N1 = int(input())
+    S1 = set(map(int, input().split()))
+    N1 = int(input())
+    S2 = set(map(int, input().split()))
+    print(S1.issubset(S2))
